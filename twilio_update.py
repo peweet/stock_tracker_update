c312c6fcad6934b636ffd3ce27f8983d9ea653c9
Patrick Glynn <p.glynn18@gmail.com>
2025-04-23 11:46:04 +0200

    fix API key error

--- twilio_update.py
+++ twilio_update.py
@@ -4,21 +4,8 @@ import os
 load_dotenv()
 class TwilioSMS:
     def __init__(self):
-        # Find your Account SID and Auth Token at twilio.com/console
-        # and set the environment variables. See http://twil.io/secure
         self.account_sid = os.environ["TWILIO_ACCOUNT_SID"]
         self.auth_token = os.environ["TWILIO_AUTH_TOKEN"]
-
-    """
-    TSLA: 🔺2%
-    Headline: Were Hedge Funds Right About Piling Into Tesla Inc. (TSLA)?. 
-    Brief: We at Insider Monkey have gone over 821 13F filings that hedge funds and prominent investors are required to file by the SEC The 13F filings show the funds' and investors' portfolio positions as of March 31st, near the height of the coronavirus market crash.
-    or
-    "TSLA: 🔻5%
-    Headline: Were Hedge Funds Right About Piling Into Tesla Inc. (TSLA)?. 
-    Brief: We at Insider Monkey have gone over 821 13F filings that hedge funds and prominent investors are required to file by the SEC The 13F filings show the funds' and investors' portfolio positions as of March 31st, near the height of the coronavirus market crash.
-    """
-
     def sms_message(self, input_body):
         client = Client(self.account_sid, self.auth_token)
         message = client.messages.create(
